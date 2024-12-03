7c8d984a8ebc5f006178cf21c5a8d2cd7abf4106
rishi <rishid23@iitk.ac.in>
2024-12-03 01:06:24 +0000

    restart

--- RACCOON/Agents/LATS/TreeState.py
+++
@@ -1,8 +0,0 @@
-from typing_extensions import TypedDict
-from Agents.LATS.Reflection import Node
-
-class TreeState(TypedDict):
-    # The full tree
-    root: Node
-    # The original input
-    input: str
